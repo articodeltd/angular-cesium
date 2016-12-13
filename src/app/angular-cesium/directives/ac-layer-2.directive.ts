8d3332f3055ce598372e48f31267138740c7b405
tomermoshe <tomosher1@gmail.com>
2016-12-13 17:37:28 +0200

    First commit - layer

--- src/app/angular-cesium/directives/ac-layer-2.directive.ts
+++ src/app/angular-cesium/directives/ac-layer-2.directive.ts
@@ -1,6 +1,9 @@
 import { Observable } from 'rxjs';
 import { LayerService } from './../services/layer-service/layer-service.service';
-import { Directive, TemplateRef, ViewContainerRef, OnInit, ChangeDetectorRef, Input } from '@angular/core';
+import {
+  Directive, TemplateRef, ViewContainerRef, OnInit, ChangeDetectorRef, Input, OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 export class LayerContext {
   constructor(private data: Object) {
@@ -17,28 +20,23 @@ export class LayerContext {
 })
 export class AcLayer2Directive implements OnInit {
 
-  @Input()
-  acLayer2Of : Observable<any>
-
-  @Input()
-  entity: string;
-  
+  /*@Input()
+  acLayer2Of : Observable<any>;
+*/
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainerRef: ViewContainerRef,
-    private changeDetector : ChangeDetectorRef , 
+    private changeDetector : ChangeDetectorRef ,
     private layerService: LayerService
   ) { }
 
   ngOnInit() {
-
     this.viewContainerRef.createEmbeddedView(
-          this.templateRef
-        //   new LayerContext({
-        //   getImage: () => "/assets/bear-tongue_1558824i.jpg",
-        //   getPosition: () => Cesium.Cartesian3.fromDegrees(-25.59777, 80.03883)
-        // })
+          this.templateRef,
+          new LayerContext({
+          getImage: () => "/assets/bear-tongue_1558824i.jpg",
+          getPosition: () => Cesium.Cartesian3.fromDegrees(-25.59777, 80.03883)
+        })
         );
-        let doda = 'sack';
   }
 }
