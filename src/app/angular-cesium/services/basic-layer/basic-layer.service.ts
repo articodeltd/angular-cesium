18324aff43e8c1ee3f3b837d64665806ee0588a3
gonengar <gonengar@gmail.com>
2016-12-25 23:35:52 +0200

    Wrapped the hook of basic-layer in order to prevent the user from overriding it

--- src/app/angular-cesium/services/basic-layer/basic-layer.service.ts
+++ src/app/angular-cesium/services/basic-layer/basic-layer.service.ts
@@ -1,16 +1,25 @@
 import {ContentChildren, QueryList, AfterContentInit, ViewChildren, AfterViewInit} from "@angular/core";
 import {AcLayerComponent} from "../../components/ac-layer/ac-layer.component";
 
-export abstract class BasicLayer implements AfterViewInit, AfterContentInit{
+export abstract class BasicLayer implements AfterViewInit {
     @ViewChildren(AcLayerComponent) acLayersView: QueryList<AcLayerComponent>;
     @ContentChildren(AcLayerComponent) acLayersContent: QueryList<AcLayerComponent>;
-    constructor() {}
 
-    ngAfterViewInit(): void {
-        this.acLayersView.forEach((layer) => layer.init(this));
+    constructor() {
+        //Used to prevent overriding of our core function
+        let originalFunc = this.ngAfterViewInit;
+        this.ngAfterViewInit = () => {
+            originalFunc();
+            this.initAcLayersView()
+        };
     }
 
-    ngAfterContentInit(): void {
-        this.acLayersContent.forEach((layer) => layer.init(this));
+    ngAfterViewInit(): void {
+        //no op
+    }
+
+    private initAcLayersView(): void {
+        console.log('basic layer');
+        this.acLayersView.forEach((layer) => layer.init(this));
     }
 }
