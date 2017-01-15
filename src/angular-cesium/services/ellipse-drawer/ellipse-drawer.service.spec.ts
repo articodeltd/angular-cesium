aac08cf1ee4eb6e0839dcbdd24f493ff8999dc85
gotenxds <david.limkys@gmail.com>
2017-01-15 15:50:00 +0200

    Added an ellipse drawer.

---
+++ src/angular-cesium/services/ellipse-drawer/ellipse-drawer.service.spec.ts
@@ -0,0 +1,15 @@
+
+import { TestBed, inject } from '@angular/core/testing';
+import { EllipseDrawerService } from './ellipse-drawer.service';
+
+describe('EllipseDrawerService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [EllipseDrawerService]
+    });
+  });
+
+  it('should ...', inject([EllipseDrawerService], (service: EllipseDrawerService) => {
+    expect(service).toBeTruthy();
+  }));
+});
