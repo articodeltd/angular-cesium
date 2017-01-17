64c620081509a4522d56a7312df9ac87a61d8e63
gotenxds <david.limkys@gmail.com>
2017-01-17 15:39:36 +0200

    Changed name to dynamic-ellipse-drawer.service

---
+++ src/angular-cesium/services/ellipse-drawer/dynamic-ellipse-drawer.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core';
+import {SimpleDrawerService} from "../simple-drawer/simple-drawer.service";
+import {CesiumService} from "../cesium/cesium.service";
+
+@Injectable()
+export class DynamicEllipseDrawerService extends SimpleDrawerService {
+    constructor(cesiumService: CesiumService) {
+        super(Cesium.PolylineCollection, cesiumService);
+    }
+
+    add(id: number, cesiumProps: any): any {
+        let positions = DynamicEllipseDrawerService.generatePositions(cesiumProps);
+
+        let polyline = this.cesiumCollection.add({
+            positions: positions,
+            width: 1
+        });
+
+        polyline.id = id;
+        polyline.center = cesiumProps.center;
+
+        return polyline;
+    }
+
+    update(id: number, cesiumProps: Object): any {
+        let ellipse = this.getPrimitiveById(id);
+
+        ellipse.positions = DynamicEllipseDrawerService.generatePositions(cesiumProps);
+    }
+
+
+    private static generatePositions(cesiumProps: any) {
+        let points = Cesium.EllipseGeometryLibrary.computeEllipsePositions(cesiumProps, false, true);
+        let positions = [];
+
+        for (let startIndex = 0; startIndex < points.outerPositions.length; startIndex += 3) {
+            positions.push(Cesium.Cartesian3.fromArray(points.outerPositions, startIndex));
+        }
+
+        positions.push(Cesium.Cartesian3.fromArray(points.outerPositions));
+
+        return positions;
+    }
+}
