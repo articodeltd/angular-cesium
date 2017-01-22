df402b7dbbabb5794f9f51eeffc3341c39b5db95
gotenxds <david.limkys@gmail.com>
2017-01-22 10:02:06 +0200

    tracks-layer.component now uses now services.

--- src/app/tracks-layer/tracks-layer.component.ts
+++ src/app/tracks-layer/tracks-layer.component.ts
@@ -1,14 +1,14 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
 import {Observable} from "rxjs";
 import {AcNotification} from "../../angular-cesium/models/ac-notification";
-import {ActionType} from "../../angular-cesium/models/action-type.enum";
-import {AsyncService} from "../../utils/services/async/async.service";
 import {AcLayerComponent} from "../../angular-cesium/components/ac-layer/ac-layer.component";
+import {TracksDataProvider} from "../../utils/services/dataProvider/tracksDataProvider.service";
 
 @Component({
     selector: 'tracks-layer',
     templateUrl: './tracks-layer.component.html',
-    styleUrls: ['./tracks-layer.component.css']
+    styleUrls: ['./tracks-layer.component.css'],
+    providers:[TracksDataProvider]
 })
 export class TracksLayerComponent implements OnInit {
     @ViewChild(AcLayerComponent) layer: AcLayerComponent;
@@ -17,37 +17,11 @@ export class TracksLayerComponent implements OnInit {
     Cesium = Cesium;
     showTracks = true;
 
-    constructor(private asyncService: AsyncService) {
+    constructor(private tracksDataProvider: TracksDataProvider) {
     }
 
     ngOnInit() {
-        let socket = io.connect('http://localhost:3000');
-        this.tracks$ = Observable.create((observer) => {
-            socket.on('birds', (data) => {
-                this.asyncService.forEach(
-                    data,
-                    (acNotification) => {
-                        let action;
-                        if (acNotification.action === "ADD_OR_UPDATE") {
-                            action = ActionType.ADD_UPDATE;
-                        }
-                        else if (acNotification.action === "DELETE") {
-                            action = ActionType.DELETE
-                        }
-                        acNotification.actionType = action;
-                        acNotification.entity = this.convertToCesiumObj(acNotification.entity);
-                        observer.next(acNotification);
-                    },
-                    2000);
-            });
-        });
-    }
-
-    convertToCesiumObj(entity): any {
-        entity.scale = entity.id === 1 ? 0.3 : 0.15;
-        entity.color = entity.id === 1 ? Cesium.Color.RED : undefined;
-        entity.position = Cesium.Cartesian3.fromDegrees(entity.position.long, entity.position.lat);
-        return entity;
+        this.tracks$ = this.tracksDataProvider.get();
     }
 
     removeAll() {
