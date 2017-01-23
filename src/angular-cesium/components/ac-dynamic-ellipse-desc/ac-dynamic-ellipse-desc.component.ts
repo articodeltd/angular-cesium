8e73736a1c8f84668e03f0ac63126356c6da4627
Tomer Moshe <tomosher1@gmail.com>
2017-01-23 07:42:45 +0200

    david dynamic ellipse branch

---
+++ src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.ts
@@ -0,0 +1,38 @@
+import {Component} from "@angular/core";
+import {CesiumProperties} from "../../services/cesium-properties/cesium-properties.service";
+import {ComputationCache} from "../../services/computation-cache/computation-cache.service";
+import {LayerService} from "../../services/layer-service/layer-service.service";
+import {BasicDesc} from "../../services/basic-desc/basic-desc.service";
+import {DynamicEllipseDrawerService} from "../../services/ellipse-drawer/dynamic-ellipse-drawer.service";
+
+/**
+ *  This is a dynamic(position is updatable) implementation of an ellipse.
+ *  Usage :
+ *    <ac-dynamic-ellipse-desc props="{
+        center: data.position,
+        semiMajorAxis:250000.0,
+        semiMinorAxis:400000.0,
+        rotation : 0.785398,
+        width:3, // Optional
+        granularity:0.08 // Optional
+        }">
+    }">
+
+ @param {Cesium.Cartesian3} center
+ @param {number} semiMajorAxis
+ @param {number} semiMinorAxis
+ @param {number} rotation
+ @param {number} [1] width
+ @param {number} granularity
+ */
+@Component({
+    selector: 'ac-dynamic-ellipse-desc',
+    templateUrl: 'ac-dynamic-ellipse-desc.component.html',
+    styleUrls: ['ac-dynamic-ellipse-desc.component.css']
+})
+export class AcDynamicEllipseDescComponent extends BasicDesc {
+    constructor(ellipseDrawer: DynamicEllipseDrawerService, layerService: LayerService,
+                computationCache: ComputationCache, cesiumProperties: CesiumProperties) {
+        super(ellipseDrawer, layerService, computationCache, cesiumProperties);
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import {AppComponent} from "./app.component";
 import {AngularCesiumModule} from "../angular-cesium/angular-cesium.module";
 import {PerformanceFormComponent} from "./performence-test/performance-form/performance-form.component";
 import {TracksLayerComponent} from "./tracks-layer/tracks-layer.component";
-import { AcLabelDescComponent } from '../angular-cesium/components/ac-label-desc/ac-label-desc.component';
 import { BaseLayerComponent } from './base-layer/base-layer.component';
+import {EllipseLayerComponent} from "./ellipse-layer/ellipse-layer.component";
 
 @NgModule({
     declarations: [
-        AppComponent,PerformanceFormComponent,TracksLayerComponent, BaseLayerComponent
+        AppComponent,PerformanceFormComponent,TracksLayerComponent, EllipseLayerComponent, BaseLayerComponent
     ],
     imports: [
         BrowserModule,
