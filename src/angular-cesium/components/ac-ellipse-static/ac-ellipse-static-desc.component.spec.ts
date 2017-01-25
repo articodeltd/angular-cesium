048618fc27b030dabe375e02fe5bda3914063ce2
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-01-25 12:01:07 +0200

    Static ellipse desc

---
+++ src/angular-cesium/components/ac-ellipse-static/ac-ellipse-static-desc.component.spec.ts
@@ -0,0 +1,26 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AcEllipseStaticDescComponent } from './ac-ellipse-static-desc.component';
+
+describe('AcEllipseStaticDescComponent', () => {
+	let component: AcEllipseStaticDescComponent;
+	let fixture: ComponentFixture<AcEllipseStaticDescComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [AcEllipseStaticDescComponent]
+		})
+			.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AcEllipseStaticDescComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
