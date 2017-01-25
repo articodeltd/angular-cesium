c2fed034a53d1740301f923bb9653b6f38b8e11e
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-01-25 12:22:47 +0200

    Ellipse layer - for ui tests

---
+++ src/app/ellipse-layer/ellipse-layer.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AcNotification } from '../../angular-cesium/models/ac-notification';
+import { ActionType } from '../../angular-cesium/models/action-type.enum';
+import { AcLayerComponent } from '../../angular-cesium/components/ac-layer/ac-layer.component';
+import { AsyncService } from "../../utils/services/async/async.service";
+
+@Component({
+	selector: 'ellipse-layer',
+	templateUrl: './ellipse-layer.component.html',
+	styleUrls: ['./ellipse-layer.component.css']
+})
+export class EllipseLayerComponent implements OnInit, AfterViewInit {
+	@ViewChild(AcLayerComponent) layer: AcLayerComponent;
+
+	ellipses$: Observable<AcNotification>;
+
+	constructor(private asyncService: AsyncService) {
+	}
+
+	ngOnInit() {
+		let socket = io.connect('http://localhost:3000');
+		this.ellipses$ = Observable.create((observer) => {
+			socket.on('birds', (data) => {
+				this.asyncService.forEach(
+					data,
+					(acEntity) => {
+						let action;
+						if (acEntity.action === "ADD_OR_UPDATE") {
+							action = ActionType.ADD_UPDATE;
+						}
+						else if (acEntity.action === "DELETE") {
+							action = ActionType.DELETE
+						}
+						acEntity.actionType = action;
+						acEntity.entity = this.convertToCesiumObj(acEntity);
+						observer.next(acEntity);
+					},
+					2000);
+			});
+		})
+	}
+
+	oneAndOnlyMaterial = new Cesium.PerInstanceColorAppearance({
+		translucent: false,
+		closed: true
+	});
+
+	convertToCesiumObj(data): any {
+		return {
+			geometry: {
+				center: Cesium.Cartesian3.fromRadians(Math.random(), Math.random()),
+				semiMajorAxis: 500000.0,
+				semiMinorAxis: 300000.0,
+				height: 15000.0,
+				rotation: Cesium.Math.toRadians(45)
+			},
+			attributes: {
+				color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromRandom())
+			},
+			appearance: this.oneAndOnlyMaterial
+		}
+	}
+
+	removeAll() {
+		this.layer.removeAll();
+	}
+
+	ngAfterViewInit() {
+	}
+}
