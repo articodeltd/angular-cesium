7ea58181fcaea64babbad3f595a50c890daedbbe
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-01-25 11:47:39 +0200

    Basic static primitive desc

--- src/angular-cesium/services/basic-desc/basic-desc.service.ts
+++ src/angular-cesium/services/basic-desc/basic-desc.service.ts
@@ -1,52 +1,52 @@
-import {OnInit, Input} from "@angular/core";
-import {LayerService} from "../layer-service/layer-service.service";
-import {SimpleDrawerService} from "../simple-drawer/simple-drawer.service";
-import {ComputationCache} from "../computation-cache/computation-cache.service";
-import {CesiumProperties} from "../cesium-properties/cesium-properties.service";
+import { OnInit, Input } from "@angular/core";
+import { LayerService } from "../layer-service/layer-service.service";
+import { SimpleDrawerService } from "../simple-drawer/simple-drawer.service";
+import { ComputationCache } from "../computation-cache/computation-cache.service";
+import { CesiumProperties } from "../cesium-properties/cesium-properties.service";
 
 export class BasicDesc implements OnInit {
-    @Input()
-    props: any;
+	@Input()
+	props: any;
 
-    private _primitiveMap = new Map();
-    private _propsEvaluateFn: Function;
+	protected _primitiveMap = new Map();
 
-    constructor(private _drawer: SimpleDrawerService,
-                private _layerService: LayerService,
-                private _computationCache: ComputationCache,
-                private _cesiumProperties: CesiumProperties,
-    ) {}
+	private _propsEvaluateFn: Function;
 
-    _propsEvaluator(context: Object): any {
-        return this._propsEvaluateFn(this._computationCache, context);
-    }
+	constructor(protected _drawer: SimpleDrawerService,
+	            protected _layerService: LayerService,
+	            protected _computationCache: ComputationCache,
+	            protected _cesiumProperties: CesiumProperties,) {
+	}
 
-    ngOnInit(): void {
-        this._layerService.registerDescription(this);
-        this._propsEvaluateFn = this._cesiumProperties.createEvaluator(this.props);
-        this._drawer.setPropsAssigner(this._cesiumProperties.createAssigner(this.props));
-    }
+	private _propsEvaluator(context: Object): any {
+		return this._propsEvaluateFn(this._computationCache, context);
+	}
 
-    draw(context, id): any {
-        const cesiumProps = this._propsEvaluator(context);
-        if (!this._primitiveMap.has(id)) {
-            const primitive = this._drawer.add(cesiumProps);
-            this._primitiveMap.set(id, primitive);
-        } else {
-            let primitive = this._primitiveMap.get(id);
-            primitive = this._drawer.update(primitive, cesiumProps);
-            this._primitiveMap.set(id, primitive);
-        }
-    }
+	ngOnInit(): void {
+		this._layerService.registerDescription(this);
+		this._propsEvaluateFn = this._cesiumProperties.createEvaluator(this.props);
+		this._drawer.setPropsAssigner(this._cesiumProperties.createAssigner(this.props));
+	}
 
-    remove(id){
-        const primitive = this._primitiveMap.get(id);
-        this._drawer.remove(primitive);
-        this._primitiveMap.delete(id);
-    }
+	draw(context, id): any {
+		const cesiumProps = this._propsEvaluator(context);
+		if (!this._primitiveMap.has(id)) {
+			const primitive = this._drawer.add(cesiumProps);
+			this._primitiveMap.set(id, primitive);
+		} else {
+			const primitive = this._primitiveMap.get(id);
+			this._drawer.update(primitive, cesiumProps);
+		}
+	}
 
-    removeAll(){
-        this._primitiveMap.clear();
-        this._drawer.removeAll();
-    }
+	remove(id) {
+		const primitive = this._primitiveMap.get(id);
+		this._drawer.remove(primitive);
+		this._primitiveMap.delete(id);
+	}
+
+	removeAll() {
+		this._primitiveMap.clear();
+		this._drawer.removeAll();
+	}
 }
---
+++ src/angular-cesium/services/basic-primitive-desc/basic-static-primitive-desc.service.ts
@@ -0,0 +1,48 @@
+import { Input } from '@angular/core';
+
+import { BasicDesc } from '../basic-desc/basic-desc.service';
+import { LayerService } from '../layer-service/layer-service.service';
+import { ComputationCache } from '../computation-cache/computation-cache.service';
+import { CesiumProperties } from '../cesium-properties/cesium-properties.service';
+import { StaticPrimitiveDrawer } from '../static-primitive-drawer/static-primitive-drawer.service';
+
+export class BasicStaticPrimitiveDesc extends BasicDesc {
+	@Input()
+	geometryProps: any;
+	@Input()
+	instanceProps: any;
+	@Input()
+	primitiveProps: any;
+
+	private _geometryPropsFn: Function;
+	private _instancePropsFn: Function;
+	private _primitivePropsFn: Function;
+
+	constructor(protected _staticPrimitiveDrawer: StaticPrimitiveDrawer, layerService: LayerService,
+	            computationCache: ComputationCache, cesiumProperties: CesiumProperties) {
+		super(_staticPrimitiveDrawer, layerService, computationCache, cesiumProperties);
+	}
+
+	ngOnInit(): void {
+		this._layerService.registerDescription(this);
+		this._geometryPropsFn = this._cesiumProperties.createEvaluator(this.geometryProps);
+		this._instancePropsFn = this._cesiumProperties.createEvaluator(this.instanceProps);
+		this._primitivePropsFn = this._cesiumProperties.createEvaluator(this.primitiveProps);
+	}
+
+	draw(context, id): any {
+		if (!this._primitiveMap.has(id)) {
+			const geometryProps = this._specificPropsEvaluator(context, this._geometryPropsFn);
+			const instanceProps = this._specificPropsEvaluator(context, this._instancePropsFn);
+			const primitiveProps = this._specificPropsEvaluator(context, this._primitivePropsFn);
+
+			const primitive = this._staticPrimitiveDrawer.add(geometryProps, instanceProps, primitiveProps);
+
+			this._primitiveMap.set(id, primitive);
+		}
+	}
+
+	private _specificPropsEvaluator(context: Object, propFn: Function) {
+		return propFn(this._computationCache, context);
+	}
+}
