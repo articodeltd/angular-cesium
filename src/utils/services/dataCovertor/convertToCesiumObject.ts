a9b26876bb4b78a66193beb50743d0577886f083
Shilo Drot <shilodrot@gmail.com>
2017-01-30 16:27:34 +0200

    Add performance test to dynamic polyline

--- src/utils/services/dataCovertor/convertToCesiumObject.ts
+++ src/utils/services/dataCovertor/convertToCesiumObject.ts
@@ -3,6 +3,15 @@ let oneAndOnlyMaterial = new Cesium.PerInstanceColorAppearance({
 	closed: true
 });
 
+let yellowMatirial = new Cesium.Material({
+	fabric : {
+		type : 'Color',
+		uniforms : {
+			color : new Cesium.Color(1.0, 1.0, 0.0, 1.0)
+		}
+	}
+});
+
 export function convertToCesiumObj(entity): any {
 	entity.scale = entity.id === 1 ? 0.3 : 0.15;
 	entity.color = entity.id === 1 ? Cesium.Color.RED : undefined;
@@ -51,8 +60,17 @@ export function convertToCesiumObj(entity): any {
 			closed: true,
 			translucent: false
 		})
-	}
-	;
+	};
+
+	entity.dynamicPolyline = {
+		width : 2,
+		positions: Cesium.Cartesian3.fromDegreesArray(
+			[
+				Math.floor(Math.random() * 50), Math.floor(Math.random() * 50),
+				Math.floor(Math.random() * 50), Math.floor(Math.random() * 50)
+			]),
+		material: yellowMatirial
+	};
 
 	return entity;
 }
