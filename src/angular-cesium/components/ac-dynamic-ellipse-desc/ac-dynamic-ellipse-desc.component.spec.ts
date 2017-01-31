a7d27e46aa08a9b2ee446f7c486200454b2d5942
gotenxds <david.limkys@gmail.com>
2017-01-31 14:48:04 +0200

    Now using testUtils.

--- src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
+++ src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
@@ -6,14 +6,12 @@ import { LayerService } from '../../services/layer-service/layer-service.service
 import { ComputationCache } from '../../services/computation-cache/computation-cache.service';
 import { CesiumProperties } from '../../services/cesium-properties/cesium-properties.service';
 import { CesiumService } from '../../services/cesium/cesium.service';
+import { mockProvider, providerFromMock } from '../../utils/testingUtils';
 
-describe('AcDynamicEllipseDescComponent', () => {
+fdescribe('AcDynamicEllipseDescComponent', () => {
     let component: AcDynamicEllipseDescComponent;
     let fixture: ComponentFixture<AcDynamicEllipseDescComponent>;
 
-    const layerService = mock(LayerService);
-    const computationCache = mock(ComputationCache);
-    const cesiumProperties = mock(CesiumProperties);
     const cesiumService = mock(CesiumService);
     const primitiveCollection = mock(Cesium.PrimitiveCollection);
 
@@ -23,10 +21,10 @@ describe('AcDynamicEllipseDescComponent', () => {
         TestBed.configureTestingModule({
             declarations: [AcDynamicEllipseDescComponent],
             providers: [DynamicEllipseDrawerService,
-                        {provide: CesiumService, useValue: instance(cesiumService)},
-                        {provide: LayerService, useValue: instance(layerService)},
-                        {provide: CesiumProperties, useValue: instance(cesiumProperties)},
-                        {provide: ComputationCache, useValue: instance(computationCache)}]
+                        providerFromMock(CesiumService, cesiumService),
+                        mockProvider(LayerService),
+                        mockProvider(CesiumProperties),
+                        mockProvider(ComputationCache)]
         })
             .compileComponents();
     }));
