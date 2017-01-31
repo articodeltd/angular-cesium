88e3228c138f42b807f1306981c3e6c46e2fa676
shayalaluf <shayalalu@gmail.com>
2017-01-31 14:39:16 +0200

    Merge branch 'static-circle' of https://bitbucket.org/tgft/angular-cesium into static-polyline
    # Conflicts:
    #	src/angular-cesium/angular-cesium.module.ts
    #	src/angular-cesium/components/ac-layer/ac-layer.component.ts
    #	src/app/app.component.html
    #	src/app/app.module.ts
    #	src/utils/services/dataCovertor/convertToCesiumObject.ts

--- src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
+++ src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
@@ -1,24 +1,24 @@
-import {async, ComponentFixture, TestBed} from "@angular/core/testing";
-import {AcDynamicEllipseDescComponent} from "./ac-dynamic-ellipse-desc.component";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { AcDynamicEllipseDescComponent } from "./ac-dynamic-ellipse-desc.component";
 
 describe('AcDynamicEllipseDescComponent', () => {
-  let component: AcDynamicEllipseDescComponent;
-  let fixture: ComponentFixture<AcDynamicEllipseDescComponent>;
+	let component: AcDynamicEllipseDescComponent;
+	let fixture: ComponentFixture<AcDynamicEllipseDescComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AcDynamicEllipseDescComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [AcDynamicEllipseDescComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AcDynamicEllipseDescComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AcDynamicEllipseDescComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
