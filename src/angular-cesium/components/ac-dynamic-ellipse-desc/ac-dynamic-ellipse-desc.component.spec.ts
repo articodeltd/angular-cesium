b4e56be8b373e58fec3af32bd93bcdad23d6910b
gotenxds <david.limkys@gmail.com>
2017-01-31 13:19:08 +0200

    Fixed standards.

--- src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
+++ src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
@@ -1,11 +1,11 @@
-import {async, ComponentFixture, TestBed} from "@angular/core/testing";
-import {AcDynamicEllipseDescComponent} from "./ac-dynamic-ellipse-desc.component";
-import {DynamicEllipseDrawerService} from "../../services/ellipse-drawer/dynamic-ellipse-drawer.service";
-import {mock, instance, when, anything} from "ts-mockito";
-import {LayerService} from "../../services/layer-service/layer-service.service";
-import {ComputationCache} from "../../services/computation-cache/computation-cache.service";
-import {CesiumProperties} from "../../services/cesium-properties/cesium-properties.service";
-import {CesiumService} from "../../services/cesium/cesium.service";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AcDynamicEllipseDescComponent } from './ac-dynamic-ellipse-desc.component';
+import { DynamicEllipseDrawerService } from '../../services/ellipse-drawer/dynamic-ellipse-drawer.service';
+import { mock, instance, when } from 'ts-mockito';
+import { LayerService } from '../../services/layer-service/layer-service.service';
+import { ComputationCache } from '../../services/computation-cache/computation-cache.service';
+import { CesiumProperties } from '../../services/cesium-properties/cesium-properties.service';
+import { CesiumService } from '../../services/cesium/cesium.service';
 
 describe('AcDynamicEllipseDescComponent', () => {
     let component: AcDynamicEllipseDescComponent;
