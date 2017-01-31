1efbbaa5e1d351dc36516d55506d0067512f23a2
gotenxds <david.limkys@gmail.com>
2017-01-31 13:16:01 +0200

    Fixed tests.

--- src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
+++ src/angular-cesium/components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component.spec.ts
@@ -1,24 +1,43 @@
 import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 import {AcDynamicEllipseDescComponent} from "./ac-dynamic-ellipse-desc.component";
+import {DynamicEllipseDrawerService} from "../../services/ellipse-drawer/dynamic-ellipse-drawer.service";
+import {mock, instance, when, anything} from "ts-mockito";
+import {LayerService} from "../../services/layer-service/layer-service.service";
+import {ComputationCache} from "../../services/computation-cache/computation-cache.service";
+import {CesiumProperties} from "../../services/cesium-properties/cesium-properties.service";
+import {CesiumService} from "../../services/cesium/cesium.service";
 
 describe('AcDynamicEllipseDescComponent', () => {
-  let component: AcDynamicEllipseDescComponent;
-  let fixture: ComponentFixture<AcDynamicEllipseDescComponent>;
+    let component: AcDynamicEllipseDescComponent;
+    let fixture: ComponentFixture<AcDynamicEllipseDescComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AcDynamicEllipseDescComponent ]
-    })
-    .compileComponents();
-  }));
+    const layerService = mock(LayerService);
+    const computationCache = mock(ComputationCache);
+    const cesiumProperties = mock(CesiumProperties);
+    const cesiumService = mock(CesiumService);
+    const primitiveCollection = mock(Cesium.PrimitiveCollection);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AcDynamicEllipseDescComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    when(cesiumService.getScene()).thenReturn({primitives: instance(primitiveCollection)});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AcDynamicEllipseDescComponent],
+            providers: [DynamicEllipseDrawerService,
+                        {provide: CesiumService, useValue: instance(cesiumService)},
+                        {provide: LayerService, useValue: instance(layerService)},
+                        {provide: CesiumProperties, useValue: instance(cesiumProperties)},
+                        {provide: ComputationCache, useValue: instance(computationCache)}]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AcDynamicEllipseDescComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
