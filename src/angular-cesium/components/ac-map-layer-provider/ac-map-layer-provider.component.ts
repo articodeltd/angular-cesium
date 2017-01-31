30ba445c695757d2ccb49617321bd1c75052917e
Tomer Moshe <tomosher1@gmail.com>
2017-01-31 03:55:48 +0200

    added wms layer ability

--- src/angular-cesium/components/ac-map-layer-provider/ac-map-layer-provider.component.ts
+++ src/angular-cesium/components/ac-map-layer-provider/ac-map-layer-provider.component.ts
@@ -2,21 +2,40 @@ import {Component, OnInit, Input} from '@angular/core';
 import {CesiumService} from "../../services/cesium/cesium.service";
 
 @Component({
-  selector: 'ac-map-layer-provider',
-  templateUrl: 'ac-map-layer-provider.component.html',
-  styleUrls: ['ac-map-layer-provider.component.css']
+    selector: 'ac-map-layer-provider',
+    templateUrl: 'ac-map-layer-provider.component.html',
+    styleUrls: ['ac-map-layer-provider.component.css']
 })
 export class AcMapLayerProviderComponent implements OnInit {
-  @Input()
-  url: string;
-  @Input()
-  layers: string;
-  @Input()
-  srs: string;
 
-  constructor(private cesiumService: CesiumService) { }
+    @Input()
+    url: string;
+    @Input()
+    layers: string;
+    @Input()
+    srs: string;
+    @Input()
+    format: string;
 
-  ngOnInit() {
-  }
+    constructor(private cesiumService: CesiumService) {
+        this.createWMSLayer();
+    }
+
+    private createWMSLayer() {
+        const provider = new Cesium.WebMapServieImageryProvider({
+            url: this.url,
+            layers: this.layers,
+            parameters:{
+                transparent: true,
+                format: this.format,
+                srs: this.srs
+            }
+        });
+
+        this.cesiumService.getScene().imageryLayers.addImageryProvider(provider);
+    }
+
+    ngOnInit() {
+    }
 
 }
