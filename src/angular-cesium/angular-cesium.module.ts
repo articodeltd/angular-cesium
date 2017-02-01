e3dc95019a4591e3fe74428cd999853ae2f3d059
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-02-01 13:13:49 +0200

    Change to ac-static-ellipse-desc tag

--- src/angular-cesium/angular-cesium.module.ts
+++ src/angular-cesium/angular-cesium.module.ts
@@ -5,7 +5,7 @@ import { AcMapComponent } from './components/ac-map/ac-map.component';
 import { AcLayerComponent } from './components/ac-layer/ac-layer.component';
 import { AcBillboardComponent } from './components/ac-billboard/ac-billboard.component';
 import { AcBillboardDescComponent } from './components/ac-billborad-desc/ac-billborad-desc.component';
-import { AcEllipseStaticDescComponent } from './components/ac-ellipse-static/ac-ellipse-static-desc.component';
+import { AcStaticEllipseDescComponent } from './components/ac-static-ellipse/ac-static-ellipse-desc.component';
 import { AcDynamicEllipseDescComponent } from './components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component';
 import { AcDynamicPolylineDescComponent } from './components/ac-dynamic-polyline-desc/ac-dynamic-polyline-desc.component';
 import { AcLayer2Directive } from './directives/ac-layer-2.directive';
@@ -28,7 +28,7 @@ import { UtilsModule } from '../utils/utils.module';
 		AcBillboardComponent,
 		AcBillboardDescComponent,
 		AcLabelDescComponent,
-		AcEllipseStaticDescComponent,
+		AcStaticEllipseDescComponent,
 		AcDynamicEllipseDescComponent,
 		AcDynamicPolylineDescComponent,
 		AcLayer2Directive,
@@ -38,7 +38,7 @@ import { UtilsModule } from '../utils/utils.module';
 		AcBillboardComponent,
 		AcBillboardDescComponent,
 		AcLabelDescComponent,
-		AcEllipseStaticDescComponent,
+		AcStaticEllipseDescComponent,
 		AcDynamicEllipseDescComponent,
 		AcDynamicPolylineDescComponent,
 		AcLayerComponent,
