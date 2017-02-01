140c25dd420f8385246a998b4595ef03a6587fea
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-02-01 10:37:59 +0200

    Static polygon desc tests

--- src/angular-cesium/components/ac-static-polygon-desc/ac-static-polygon-desc.component.spec.ts
+++ src/angular-cesium/components/ac-static-polygon-desc/ac-static-polygon-desc.component.spec.ts
@@ -1,21 +1,36 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AcPolygonStaticDescComponent } from './ac-polygon-static-desc.component';
+import { AcStaticPolygonDescComponent } from './ac-static-polygon-desc.component';
+import { PolygonDrawerService } from '../../services/polygon-drawer/polygon-drawer.service';
+import { mock, instance, when } from 'ts-mockito';
+import { LayerService } from '../../services/layer-service/layer-service.service';
+import { ComputationCache } from '../../services/computation-cache/computation-cache.service';
+import { CesiumProperties } from '../../services/cesium-properties/cesium-properties.service';
+import { CesiumService } from '../../services/cesium/cesium.service';
+import { mockProvider, providerFromMock } from '../../utils/testingUtils';
 
 describe('AcStaticPolygonDescComponent', () => {
-	let component: AcPolygonStaticDescComponent;
-	let fixture: ComponentFixture<AcPolygonStaticDescComponent>;
+	let component: AcStaticPolygonDescComponent;
+	let fixture: ComponentFixture<AcStaticPolygonDescComponent>;
+
+	const cesiumService = mock(CesiumService);
+	const primitiveCollection = mock(Cesium.PrimitiveCollection);
+
+	when(cesiumService.getScene()).thenReturn({primitives: instance(primitiveCollection)});
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [AcPolygonStaticDescComponent]
+			declarations: [AcStaticPolygonDescComponent],
+			providers: [PolygonDrawerService,
+				providerFromMock(CesiumService, cesiumService),
+				mockProvider(LayerService),
+				mockProvider(CesiumProperties),
+				mockProvider(ComputationCache)]
 		})
 			.compileComponents();
 	}));
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(AcPolygonStaticDescComponent);
+		fixture = TestBed.createComponent(AcStaticPolygonDescComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
