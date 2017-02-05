0edbe020de32ca09d9b44d8f0bbda7f306328acf
gonengar <gonengar@gmail.com>
2017-02-05 23:12:34 +0200

    Fix standart

--- src/angular-cesium/components/ac-arc-desc/ac-arc-desc.component.ts
+++ src/angular-cesium/components/ac-arc-desc/ac-arc-desc.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
 import {LayerService} from "../../services/layer-service/layer-service.service";
 import {CesiumProperties} from "../../services/cesium-properties/cesium-properties.service";
 import {ComputationCache} from "../../services/computation-cache/computation-cache.service";
@@ -20,15 +21,14 @@ import {BasicDesc} from "../../services/basic-desc/basic-desc.service";
  */
 
 @Component({
-  selector: 'ac-arc-desc',
-  template: ''
+    selector: 'ac-arc-desc',
+    template: ''
 })
 export class AcArcDescComponent extends BasicDesc {
 
-  constructor(arcDrawer: ArcDrawerService, layerService: LayerService,
-              computationCache: ComputationCache, cesiumProperties: CesiumProperties
-  ) {
-    super(arcDrawer, layerService, computationCache, cesiumProperties);
-  }
+    constructor(arcDrawer: ArcDrawerService, layerService: LayerService,
+                computationCache: ComputationCache, cesiumProperties: CesiumProperties) {
+        super(arcDrawer, layerService, computationCache, cesiumProperties);
+    }
 
 }
