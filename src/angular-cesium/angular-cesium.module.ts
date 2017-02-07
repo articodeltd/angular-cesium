adef9b75c0d99c0ac4868003c8d204b1296d3ede
ttsah <t.tsahi@gmail.com>
2017-02-07 11:57:20 +0200

    Fix PR

--- src/angular-cesium/angular-cesium.module.ts
+++ src/angular-cesium/angular-cesium.module.ts
@@ -1,24 +1,24 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AngularCesiumComponent} from './angular-cesium.component';
-import {AcMapComponent} from './components/ac-map/ac-map.component';
-import {AcLayerComponent} from './components/ac-layer/ac-layer.component';
-import {AcBillboardComponent} from './components/ac-billboard/ac-billboard.component';
-import {AcBillboardDescComponent} from './components/ac-billborad-desc/ac-billborad-desc.component';
-import {AcStaticEllipseDescComponent} from './components/ac-static-ellipse/ac-static-ellipse-desc.component';
-import {AcDynamicEllipseDescComponent} from './components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component';
-import {AcDynamicPolylineDescComponent} from './components/ac-dynamic-polyline-desc/ac-dynamic-polyline-desc.component';
-import {AcStaticCircleDescComponent} from './components/ac-static-circle-desc/ac-static-circle-desc.component';
-import {AcLayer2Directive} from './directives/ac-layer-2.directive';
-import {Angular2ParseModule} from '../angular2-parse/src/angular2-parse.module';
-import {PixelOffsetPipe} from './pipes/pixel-offset/pixel-offset.pipe';
-import {JsonMapper} from './services/json-mapper/json-mapper.service';
-import {CesiumProperties} from './services/cesium-properties/cesium-properties.service';
-import {AcLabelDescComponent} from './components/ac-label-desc/ac-label-desc.component';
-import {UtilsModule} from '../utils/utils.module';
-import {AcStaticPolylineDescComponent} from './components/ac-static-polyline-desc/ac-static-polyline-desc.component';
-import {GeoUtilsService} from './services/geo-utils/geo-utils.service';
-import {AcArcDescComponent} from "./components/ac-arc-desc/ac-arc-desc.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularCesiumComponent } from './angular-cesium.component';
+import { AcMapComponent } from './components/ac-map/ac-map.component';
+import { AcLayerComponent } from './components/ac-layer/ac-layer.component';
+import { AcBillboardComponent } from './components/ac-billboard/ac-billboard.component';
+import { AcBillboardDescComponent } from './components/ac-billborad-desc/ac-billborad-desc.component';
+import { AcStaticEllipseDescComponent } from './components/ac-static-ellipse/ac-static-ellipse-desc.component';
+import { AcDynamicEllipseDescComponent } from './components/ac-dynamic-ellipse-desc/ac-dynamic-ellipse-desc.component';
+import { AcDynamicPolylineDescComponent } from './components/ac-dynamic-polyline-desc/ac-dynamic-polyline-desc.component';
+import { AcStaticCircleDescComponent } from './components/ac-static-circle-desc/ac-static-circle-desc.component';
+import { AcLayer2Directive } from './directives/ac-layer-2.directive';
+import { Angular2ParseModule } from '../angular2-parse/src/angular2-parse.module';
+import { PixelOffsetPipe } from './pipes/pixel-offset/pixel-offset.pipe';
+import { JsonMapper } from './services/json-mapper/json-mapper.service';
+import { CesiumProperties } from './services/cesium-properties/cesium-properties.service';
+import { AcLabelDescComponent } from './components/ac-label-desc/ac-label-desc.component';
+import { UtilsModule } from '../utils/utils.module';
+import { AcStaticPolylineDescComponent } from './components/ac-static-polyline-desc/ac-static-polyline-desc.component';
+import { GeoUtilsService } from './services/geo-utils/geo-utils.service';
+import { AcArcDescComponent } from "./components/ac-arc-desc/ac-arc-desc.component";
 
 
 @NgModule({
@@ -57,7 +57,7 @@ import {AcArcDescComponent} from "./components/ac-arc-desc/ac-arc-desc.component
         AcStaticPolylineDescComponent,
         AcArcDescComponent
     ],
-    providers: [JsonMapper, CesiumProperties, GeoUtilsService],
+	providers: [JsonMapper, CesiumProperties, GeoUtilsService],
 })
 export class AngularCesiumModule {
 }
