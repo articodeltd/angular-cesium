09160daddc092a886499b143bd10b180b7ebed82
shayalaluf <shayalalu@gmail.com>
2017-02-13 15:37:44 +0200

    add docs

--- src/angular-cesium/services/cesium/cesium.service.ts
+++ src/angular-cesium/services/cesium/cesium.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, NgZone } from '@angular/core';
 
+/**
+ *  Service that initialize cesium viewer and expose cesium viewer and scene.
+ */
+
 @Injectable()
 export class CesiumService {
 	cesium: any;
