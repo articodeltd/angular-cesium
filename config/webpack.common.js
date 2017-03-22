7d2bdf2486f227de8a48dd854642f2bcfcdc1756
eitanfrailich <eitan.fr5@gmail.com>
2017-03-22 16:25:24 +0200

    change versions

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -18,27 +18,30 @@ module.exports = {
         loaders: [
             {
                 test: /\.ts$/,
-                loaders: ['awesome-typescript-loader?' + JSON.stringify({
-                    configFileName: 'demo/tsconfig.json'
-                }), 'angular2-template-loader']
+              loaders: [
+                {
+                  loader: 'awesome-typescript-loader',
+                  options: { configFileName: 'demo/tsconfig.json' }
+                } , 'angular2-template-loader'
+              ]
             },
             {
                 test: /\.html$/,
-                loader: 'html'
+                loader: 'html-loader'
             },
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file?name=assets/[name].[hash].[ext]'
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
             },
             {
                 test: /\.css$/,
                 exclude: [root.root('demo', 'app'), root.root('src')],
-                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+                loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
             },
             {
                 test: /\.css$/,
                 include: [root.root('demo', 'app'), root.root('src')],
-                loader: 'raw'
+                loader: 'raw-loader'
             }
         ],
 
