6674f6ec5661504e690f60d94798dda7f531f06b
Shilo Drot <shilodrot@gmail.com>
2017-03-23 14:30:52 +0200

    add update

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { WebSocketSupplier } from '../utils/services/webSocketSupplier/webSocketSupplier';
 import { MapLayerProviderOptions } from '../../src/models/map-layer-provider-options.enum';
 import { AcLabelDescComponent } from '../../src/components/ac-label-desc/ac-label-desc.component';
+import { AcDynamicPolylineDescComponent } from '../../src/components/ac-dynamic-polyline-desc/ac-dynamic-polyline-desc.component';
 
 @Component({
 	selector: 'app-root',
@@ -11,31 +12,49 @@ import { AcLabelDescComponent } from '../../src/components/ac-label-desc/ac-labe
 })
 
 export class AppComponent implements OnInit {
-	arcGisMapServerProvider = MapLayerProviderOptions.ArcGisMapServer;
-	position = Cesium.Cartesian3.fromDegrees(34.0, 32.0);
-	positions = Cesium.Cartesian3.fromDegreesArray(
-		[
-			34.1, 35.1,
-			35.1, 50.0,
-		]);
-	redMatirial = new Cesium.Material({
-		fabric : {
-			type : 'Color',
-			uniforms : {
-				color : new Cesium.Color(1.0, 0.0, 0.0, 1.0)
-			}
-		}
-	});
-	aquamarine = Cesium.Color.AQUAMARINE;
+	private arcGisMapServerProvider: MapLayerProviderOptions;
+	private position: any;
+	private positions: any;
+	private redMatirial: any;
+	private aquamarine: any;
+	private longitude: number;
+	private latitude: number;
 
 	@ViewChild(AcLabelDescComponent) label: AcLabelDescComponent;
+	@ViewChild(AcDynamicPolylineDescComponent) polyline: AcDynamicPolylineDescComponent;
+
 	constructor() {
 	}
 
 	ngOnInit() {
-		setTimeout(()=>{
+		this.longitude = 35.1;
+		this.latitude = 0.1;
+		this.arcGisMapServerProvider = MapLayerProviderOptions.ArcGisMapServer;
+		this.position = Cesium.Cartesian3.fromDegrees(34.0, 32.0);
+		this.positions = Cesium.Cartesian3.fromDegreesArray(
+			[
+				34.1, 35.1,
+				this.longitude, this.latitude
+			]);
+		this.redMatirial = new Cesium.Material({
+			fabric: {
+				type: 'Color',
+				uniforms: {
+					color: new Cesium.Color(1.0, 0.0, 0.0, 1.0)
+				}
+			}
+		});
+		this.aquamarine = Cesium.Color.AQUAMARINE;
+
+		setTimeout(() => {
 			this.label.removeFromMap();
-			console.log('remove label');
-		},10000);
+		}, 10000);
+		setInterval(() => {
+			this.positions = Cesium.Cartesian3.fromDegreesArray(
+				[
+					34.1, 35.1,
+					++this.longitude, ++this.latitude
+				]);
+		}, 500);
 	}
 }
