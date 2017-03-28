ba863f8c54b85a85716dd9927d0311743e54088f
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-03-28 11:27:28 +0300

    Removed abstract drawers - no API for extensions

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,3 @@
-export * from './basic-desc/basic-desc.service';
-export * from './basic-primitive-desc/basic-static-primitive-desc.service';
 export * from './cesium/cesium.service';
 export * from './coordinate-converter/coordinate-converter.service';
 export * from './geo-utils/geo-utils.service';
