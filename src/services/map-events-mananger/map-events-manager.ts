55fee3f180ccb00c3a1fe42c352b979513cca80f
shayAlaluf <shayalalu@gmail.com>
2017-03-30 11:43:56 +0300

    fix plonter on select

--- src/services/map-events-mananger/map-events-manager.ts
+++ src/services/map-events-mananger/map-events-manager.ts
@@ -7,7 +7,6 @@ import { DisposableObservable } from './disposable-observable';
 import { PickOptions } from './consts/pickOptions.enum';
 import { CesiumEvent } from './consts/cesium-event.enum';
 import { CesiumEventModifier } from './consts/cesium-event-modifier.enum';
-import { PlonterService } from '../plonter/plonter.service';
 
 /**
  * Manages all map events. Notice events will run outside of Angular zone
