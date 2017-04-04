c53e8f1acb7dfc1532d669932f4ee92dc004b008
shayAlaluf <shayalalu@gmail.com>
2017-04-04 14:33:23 +0300

    fix docs

--- src/services/map-events-mananger/consts/pickOptions.enum.ts
+++ src/services/map-events-mananger/consts/pickOptions.enum.ts
@@ -1,7 +1,6 @@
 /**
  *  NO_PICK,    - will not pick entities
  *  PICK_FIRST  - first entity will be picked . use Cesium.scene.pick()
- *  PICK_ONE    - in case a few entities are picked plonter is resolved . use Cesium.scene.drillPick()
  *  PICK_ALL    - all entities are picked. use Cesium.scene.drillPick()
  *  MULTI_PICK  - every selected entity is picked. If chosen twice will be unselected. use Cesium.scene.pick()
  */
