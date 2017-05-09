6997c9df6549c53f36a702d426def50aec6488b6
eitanfrailich <eitan.fr5@gmail.com>
2017-05-09 10:12:25 +0300

    dir change and style fixes

---
+++ demo/app/components/demo-map/demo-map.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewerConfiguration } from '../../../../src/services/viewer-configuration/viewer-configuration.service';
+import { AppSettingsService } from '../../services/app-settings-service/app-settings-service';
+import { MapLayerProviderOptions } from '../../../../src/models/map-layer-provider-options.enum';
+@Component({
+  selector : 'demo-map',
+  templateUrl : './demo-map.component.html',
+  providers : [],
+  styleUrls : ['./demo-map.component.css']
+})
+export class DemoMapComponent {
+  arcGisMapServerProvider = MapLayerProviderOptions.ArcGisMapServer;
+  
+  constructor(private viewerConf: ViewerConfiguration, private appSettingsService: AppSettingsService) {
+    viewerConf.viewerOptions = {
+      selectionIndicator : false,
+      timeline : false,
+      infoBox : false,
+      baseLayerPicker : false,
+      animation : false,
+      homeButton : false,
+      geocoder : false,
+      navigationHelpButton : false,
+      navigationInstructionsInitiallyVisible : false,
+    };
+    
+    viewerConf.viewerModifier = (viewer) => {
+      viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+      viewer.bottomContainer.remove();
+    };
+    
+    this.appSettingsService.showTracksLayer = true;
+	}
+}
