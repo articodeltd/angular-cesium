4d86d714ae24f49b794ebc14378155c0dac9a314
eitanfrailich <eitan.fr5@gmail.com>
2017-05-17 20:27:02 +0300

    fix relative path

--- demo/utils/apollo/apollo-client.ts
+++ demo/utils/apollo/apollo-client.ts
@@ -1,12 +1,12 @@
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
 
-// by default, this client will send queries to `/graphql` (relative to the URL of your app)
+// by default, this client will send queries to `/graphql` (relative to the URL of your app) // TODO
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: 'http://localhost:3000/graphql',
+    uri: '/graphql',
   }),
 });
 
 export function getApolloClient(): ApolloClient {
-  return client
+  return client;
 }
