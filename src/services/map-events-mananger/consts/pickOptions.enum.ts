89fb11c8458a8e970910b9fa7828c06256f1061c
shayAlaluf <shayalalu@gmail.com>
2017-07-02 14:49:00 +0300

    remove multi pick from documentation change enum to const

--- src/services/map-events-mananger/consts/pickOptions.enum.ts
+++ src/services/map-events-mananger/consts/pickOptions.enum.ts
@@ -2,9 +2,8 @@
  *  NO_PICK,    - will not pick entities
  *  PICK_FIRST  - first entity will be picked . use Cesium.scene.pick()
  *  PICK_ALL    - all entities are picked. use Cesium.scene.drillPick()
- *  MULTI_PICK  - every selected entity is picked. If chosen twice will be unselected. use Cesium.scene.pick()
  */
-export enum PickOptions {
+export const enum PickOptions {
 	NO_PICK,
 	PICK_FIRST,
 	PICK_ALL
