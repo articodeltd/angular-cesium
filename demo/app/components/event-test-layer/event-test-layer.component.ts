0d3490219fd44024b8bc19f0f0996b45cbda0497
eitanfrailich <eitan.fr5@gmail.com>
2017-08-06 12:47:36 +0300

    fix event test layer

--- demo/app/components/event-test-layer/event-test-layer.component.ts
+++ demo/app/components/event-test-layer/event-test-layer.component.ts
@@ -7,24 +7,18 @@ import { AcEntity } from '../../../../src/models/ac-entity';
 import { AcLayerComponent } from '../../../../src/components/ac-layer/ac-layer.component';
 import { CesiumEvent } from '../../../../src/services/map-events-mananger/consts/cesium-event.enum';
 import { PickOptions } from '../../../../src/services/map-events-mananger/consts/pickOptions.enum';
-import { PlonterService } from '../../../../src/services/plonter/plonter.service';
-import { GeoUtilsService } from '../../../../src/services/geo-utils/geo-utils.service';
 
 @Component({
 	selector: 'event-test-layer',
 	templateUrl: 'event-test-layer.component.html',
 	styleUrls: ['event-test-layer.component.css'],
-	providers: [GeoUtilsService]
 })
 export class EventTestLayerComponent implements OnInit {
 	@ViewChild(AcLayerComponent) layer: AcLayerComponent;
 	tracks$: Observable<AcNotification>;
 	@Output() mouseMove = new EventEmitter();
 
-	constructor(private eventManager: MapEventsManagerService,
-							public  plonterService: PlonterService,
-							private cd: ChangeDetectorRef,
-							private geoUtilsService: GeoUtilsService) {
+	constructor(private eventManager: MapEventsManagerService) {
 		const track1: AcNotification = {
 			id: '0',
 			actionType: ActionType.ADD_UPDATE,
@@ -58,7 +52,7 @@ export class EventTestLayerComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
-		// Pass event only if clicked and contains at least one entity.
+		// Pass event only if clicked
 		this.eventManager.register({ event: CesiumEvent.LEFT_CLICK }).subscribe((result) => {
 			console.log('map click', result.movement, 'primitives:', result.primitives, 'entities', result.entities);
 		});
@@ -77,8 +71,6 @@ export class EventTestLayerComponent implements OnInit {
 	}
 
 	testPlonter() {
-		// TODO remove?
-		this.plonterService.plonterChangeNotifier.subscribe(() => this.cd.detectChanges());
 		this.eventManager.register({ event: CesiumEvent.LEFT_CLICK, pick: PickOptions.PICK_ONE })
 			.map((result) => result.entities)
 			.filter(entities => entities && (entities[0].id === '1' || entities[0].id === '2'))
@@ -86,10 +78,6 @@ export class EventTestLayerComponent implements OnInit {
 				console.log('plonter result: ' + JSON.stringify(result));
 				alert('picked: ' + JSON.stringify(result));
 			});
-	}
-
-	chooseEntity(entity) {
-		this.plonterService.resolvePlonter(entity);
 	}
 
 	testLongPress() {
@@ -117,7 +105,7 @@ export class EventTestLayerComponent implements OnInit {
 		const o4 = this.eventManager.register({ event: CesiumEvent.LEFT_CLICK, priority: 3 });
 		o4.subscribe((pos) => {
 			console.log('click4 Priority 3', pos.movement, 'primitives:', pos.primitives, 'entities', pos.entities);
-		},()=>console.log('error'),()=>console.log('compelete'));
+		}, () => console.log('error'), () => console.log('compelete'));
 
 		setTimeout(() => {
 			console.log('first dispose o4');
@@ -135,16 +123,8 @@ export class EventTestLayerComponent implements OnInit {
 		this.eventManager.register(inputConf).map((result) => result.entities[0]).filter((entity) => entity.id === '0').subscribe((entity) => {
 			console.log('click3', 'toggle color');
 			entity.color = entity.color === Cesium.Color.GREEN ? Cesium.Color.WHITE : Cesium.Color.GREEN;
-			this.layer.updateNotification({ actionType: ActionType.ADD_UPDATE, entity: entity, id: entity.id });
+			this.layer.update(entity, entity.id);
 		});
-		// this.eventManager.register(inputConf).subscribe((result) => {
-		// 	console.log('click3', result.movement, 'primitives:', result.primitives, 'entities', result.entities);
-		// 	if (result.entities.length === 1) {
-		// 		const entity = result.entities[0];
-		// 		entity.color = entity.color === Cesium.Color.GREEN ? Cesium.Color.WHITE : Cesium.Color.GREEN;
-		// 		this.layer.updateNotification({ actionType: ActionType.ADD_UPDATE, entity: entity, id: entity.id });
-		// 	}
-		// });
 	}
 
 }
