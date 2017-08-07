2f8457ead58841791ccb27870b52170a24c01925
eitanfrailich <eitan.fr5@gmail.com>
2017-08-07 10:15:57 +0300

    add more providers to map-provider

--- src/models/map-layer-provider-options.enum.ts
+++ src/models/map-layer-provider-options.enum.ts
@@ -1,6 +1,28 @@
 export enum MapLayerProviderOptions {
-    ArcGisMapServer,
-    WebMapTileService,
-    WebMapService,
-    OFFLINE
+  ArcGisMapServer ,
+  WebMapTileService,
+  MapTileService,
+  WebMapService,
+  SingleTileImagery,
+  OpenStreetMap,
+  BingMaps,
+  GoogleEarthImagery,
+  MapBox,
+  UrlTemplateImagery,
+  OFFLINE
 }
+
+export const MapLayerProviderOptionsToProvider = {
+  [MapLayerProviderOptions.ArcGisMapServer]: Cesium.ArcGisMapServerImageryProvider ,
+  [MapLayerProviderOptions.WebMapTileService]: Cesium.WebMapTileServiceImageryProvider,
+  [MapLayerProviderOptions.MapTileService]: Cesium.createTileMapServiceImageryProvider,
+  [MapLayerProviderOptions.WebMapService]: Cesium.WebMapServiceImageryProvider,
+  [MapLayerProviderOptions.SingleTileImagery]: Cesium.SingleTileImageryProvider,
+  
+  [MapLayerProviderOptions.OpenStreetMap]: Cesium.createOpenStreetMapImageryProvider,
+  [MapLayerProviderOptions.BingMaps]:  Cesium.BingMapsImageryProvider,
+  [MapLayerProviderOptions.GoogleEarthImagery]: Cesium.GoogleEarthImageryProvider,
+  [MapLayerProviderOptions.MapBox]: Cesium.MapboxImageryProvider,
+  [MapLayerProviderOptions.UrlTemplateImagery]: Cesium. UrlTemplateImageryProvider,
+  [MapLayerProviderOptions.OFFLINE]: '',
+};
