6fbc1676a004148a5ee3e31b10fcbd5d232b6f3c
eitanfr <>
2017-11-01 14:48:01 +0200

    circle props

--- demo/app/components/editor-layer/circles-editor-layer/circles-editor-layer.component.ts
+++ demo/app/components/editor-layer/circles-editor-layer/circles-editor-layer.component.ts
@@ -23,7 +23,7 @@ export class CirclesEditorLayerComponent implements OnInit {
 		if (this.editing$) {
 			this.stopEdit();
 		}
-		this.editing$ = this.circlesEditor.create({allowDrag: false});
+		this.editing$ = this.circlesEditor.create({allowDrag: true});
 		this.editing$.subscribe((editUpdate: CircleEditUpdate) => {
       console.log(editUpdate);
 		});
@@ -59,7 +59,6 @@ export class CirclesEditorLayerComponent implements OnInit {
 	updateCircleManually() {
 		if (this.editing$) {
 			this.editing$.setCircleManually(Cesium.Cartesian3.fromDegrees(-80, 0), 500000);
-
 		}
 	}
 }
