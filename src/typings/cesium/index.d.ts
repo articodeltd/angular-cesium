c1d7d099c800504a2926feb8c3dcffd772f99973
tomermoshe <152430tm>
2017-11-02 14:34:56 +0200

    build fix

--- src/typings/cesium/index.d.ts
+++ src/typings/cesium/index.d.ts
@@ -1,11 +1,14 @@
 declare const Cesium: any;
 
-// declare interface Cartesian2 {
-//     x: number,
-//     y: number,
-//     equals(right): boolean
-// }
-//
-// declare interface Cartesian3 extends Cartesian2 {
-//     z: number
-// }
+declare interface Cartesian2 {
+  x: number,
+  y: number,
+
+  equals(right): boolean;
+  clone(): Cartesian2;
+}
+
+declare interface Cartesian3 extends Cartesian2 {
+  z: number;
+  clone(): Cartesian3;
+}
