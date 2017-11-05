3c280f09b7290ab3653e5d3644df0e6bd93aae94
eitanfr <>
2017-11-05 18:49:03 +0200

    fix types

--- src/angular-cesium-entities-editor/models/polyline-editor-observable.ts
+++ src/angular-cesium-entities-editor/models/polyline-editor-observable.ts
@@ -2,6 +2,7 @@ import { EditPoint } from './edit-point';
 import { EditorObservable } from './editor-observable';
 import { PolylineEditUpdate } from './polyline-edit-update';
 import { PointProps, PolylineProps } from './polyline-edit-options';
+import { Cartesian3 } from '../../angular-cesium/models/cartesian3';
 
 export class PolylineEditorObservable extends EditorObservable<PolylineEditUpdate> {
   setManually: (points: {position: Cartesian3, pointProp?: PointProps}[] | Cartesian3[], polylineProps?: PolylineProps) => void;
