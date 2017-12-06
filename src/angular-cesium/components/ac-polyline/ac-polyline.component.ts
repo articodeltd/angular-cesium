2720e09d24090150a9371197907e73d1686f2d5a
nitzanmo <nitzanmousan@gmail.com>
2017-12-06 19:59:35 +0200

    lint fix

--- src/angular-cesium/components/ac-polyline/ac-polyline.component.ts
+++ src/angular-cesium/components/ac-polyline/ac-polyline.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { EntityOnMapComponent } from '../../services/entity-on-map/entity-on-map.component';
 import { MapLayersService } from '../../services/map-layers/map-layers.service';
-import {PolylinePrimitiveDrawerService} from "../../services/drawers/polyline-primitive-drawer/polyline-primitive-drawer.service";
+import {PolylinePrimitiveDrawerService} from '../../services/drawers/polyline-primitive-drawer/polyline-primitive-drawer.service';
 
 /**
  *  This is a polyline implementation.
