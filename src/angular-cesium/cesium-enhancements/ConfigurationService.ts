5277e3d851b9e908f3cc7da110c994e49282a9c5
YairTawil <yairtawil@gmail.com>
2018-08-20 16:44:24 +0300

    fix(config): change 'config' token from string to InjectionToken

--- src/angular-cesium/cesium-enhancements/ConfigurationService.ts
+++ src/angular-cesium/cesium-enhancements/ConfigurationService.ts
@@ -1,9 +1,11 @@
-import { Inject, Injectable, Optional } from '@angular/core';
+import { Inject, Injectable, Optional, InjectionToken } from '@angular/core';
 import { fixCesiumEntitiesShadows } from './StaticGeometryColorBatch';
+
+export const ANGULAR_CESIUM_CONFIG = new InjectionToken('ANGULAR_CESIUM_CONFIG');
 
 @Injectable()
 export class ConfigurationService {
-  constructor(@Optional() @Inject('config') private config: any) {
+  constructor(@Optional() @Inject(ANGULAR_CESIUM_CONFIG) private config: any) {
     const fixEntitiesShadows = config ? config.fixEntitiesShadows : true;
     if (fixEntitiesShadows !== false) {
       fixCesiumEntitiesShadows();
