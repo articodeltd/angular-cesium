8aedce5c3f2a957cdbb6c168ce6939f9ba225c0e
eitanfr <eitan.fr5@gmail.com>
2019-03-29 03:51:17 +0300

    demo WIP

--- demo/app/shared/shared.module.ts
+++ demo/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SidenavToolbarComponent } from './sidenav-toolbar/sidenav-toolbar.component';
 
 @NgModule({
   imports: [CommonModule],
   exports: [CommonModule, FormsModule],
-  declarations: [SidenavToolbarComponent]
+  declarations: []
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
