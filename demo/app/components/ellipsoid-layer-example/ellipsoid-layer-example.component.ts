3f702e173ad176977c3e8603d903bda4f1d6c28c
eitanfr <eitan.fr5@gmail.com>
2019-12-03 12:38:35 +0200

    fixes

---
+++ demo/app/components/ellipsoid-layer-example/ellipsoid-layer-example.component.ts
@@ -0,0 +1,40 @@
+import { Observable, of, of as observableOf } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { AcNotification, ActionType, CesiumService } from 'angular-cesium';
+import { MockDataProviderService } from '../../utils/services/dataProvider/mock-data-provider.service';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'ellipsoid-layer-example',
+  template: `
+    <ac-layer acFor="let entity of entities$" [context]="this" [debug]="true">
+      <ac-ellipsoid-desc props="{
+														position: entity.position,
+                            radii: defaultRadii,
+                            material: Cesium.Color.RED.withAlpha(0.5),
+                            outline: true,
+                            outlineColor: Cesium.Color.BLACK
+														}">
+      </ac-ellipsoid-desc>
+    </ac-layer>
+  `,
+  providers: []
+})
+export class EllipsoidLayerExampleComponent implements OnInit {
+
+  entities$: Observable<AcNotification>;
+  Cesium = Cesium;
+  show = true;
+  defaultRadii = new Cesium.Cartesian3(300000.0, 300000.0, 300000.0);
+
+  constructor(private dataProvider: MockDataProviderService) {
+  }
+
+  ngOnInit() {
+    this.entities$ = this.dataProvider.get$(1).pipe(map(entity => ({
+      id: entity.id,
+      actionType: ActionType.ADD_UPDATE,
+      entity: entity,
+    })));
+  }
+}
--- demo/app/components/models-layer/models-layer-example.component.ts
+++ demo/app/components/models-layer/models-layer-example.component.ts
@@ -1,73 +1,38 @@
-import { Observable, of as observableOf, Subscriber } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { AcEntity, AcNotification, ActionType } from 'angular-cesium';
-import { WebSocketSupplier } from '../../utils/services/webSocketSupplier/webSocketSupplier';
-import { TracksDataProvider } from '../../utils/services/dataProvider/tracksDataProvider.service';
+import { AcNotification, ActionType } from 'angular-cesium';
+import { MockDataProviderService } from '../../utils/services/dataProvider/mock-data-provider.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'models-layer-example',
   template: `
-    <ac-layer acFor="let track of simTracks$" [context]="this">
+    <ac-layer acFor="let track of entities$" [context]="this">
       <ac-model-desc props="{
-														position: track.position,
-														minimumPixelSize: 100,
-														maximumScale : 20000,
-														uri: '/assets/CesiumAir/Cesium_Air.gltf',
-														orientation: track.orientation
+                              position: track.position,
+                              minimumPixelSize: 100,
+                              maximumScale : 20000,
+                              uri: 'http://www.angular-cesium.com/assets/CesiumAir/Cesium_Air.gltf',
 														}">
       </ac-model-desc>
     </ac-layer>
   `,
-  providers: [TracksDataProvider]
 })
 export class ModelsLayerExampleComponent implements OnInit {
 
-  simTracks$: Observable<AcNotification> = observableOf({
-    id: '1',
-    actionType: ActionType.ADD_UPDATE,
-    entity: {}
-  });
-
+  entities$: Observable<AcNotification>;
   Cesium = Cesium;
   show = true;
 
-  constructor(webSocketSupllier: WebSocketSupplier) {
-    const socket = webSocketSupllier.get();
-    this.simTracks$ = Observable.create((observer: Subscriber<any>) => {
-      socket.on('birds', (data: any) => {
-        data.forEach(
-          (acNotification: any) => {
-            if (acNotification.action === 'ADD_OR_UPDATE') {
-              acNotification.actionType = ActionType.ADD_UPDATE;
-              acNotification.entity = new AcEntity(this.convertToCesiumObj(acNotification.entity));
-            } else if (acNotification.action === 'DELETE') {
-              acNotification.actionType = ActionType.DELETE;
-            }
-            observer.next(acNotification);
-          });
-      });
-    });
-  }
-
-  convertToCesiumObj(entity: any): any {
-
-    const fixedHeading = entity.heading - (Math.PI / 2);
-    const heading = fixedHeading;
-    const pitch = Cesium.Math.toRadians(0.0);
-    const roll = Cesium.Math.toRadians(0.0);
-
-    entity.scale = entity.id === 1 ? 0.3 : 0.15;
-    entity.alt = Math.round(entity.position.altitude);
-    entity.position = Cesium.Cartesian3.fromDegrees(entity.position.long, entity.position.lat, entity.alt);
-    entity.futurePosition =
-      Cesium.Cartesian3.fromDegrees(entity.futurePosition.long, entity.futurePosition.lat, entity.futurePosition.altitude);
-    const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
-    entity.orientation = Cesium.Transforms.headingPitchRollQuaternion(entity.position, hpr);
-
-    return entity;
+  constructor(private dataProvider: MockDataProviderService) {
   }
 
   ngOnInit() {
+    this.entities$ = this.dataProvider.get$(50).pipe(map(entity => ({
+      id: entity.id,
+      actionType: ActionType.ADD_UPDATE,
+      entity: entity,
+    })));
   }
 
 
