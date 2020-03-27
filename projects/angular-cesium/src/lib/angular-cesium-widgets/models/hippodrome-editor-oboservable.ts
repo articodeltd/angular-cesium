20bf80446915fd39f5046d7e6693e837befe5042
Boyan Stanchev <boyan_stan4ev@hotmail.com>
2020-03-27 11:36:38 +0200

    fixed HippodromeEditorObservable using wrong interface

--- projects/angular-cesium/src/lib/angular-cesium-widgets/models/hippodrome-editor-oboservable.ts
+++ projects/angular-cesium/src/lib/angular-cesium-widgets/models/hippodrome-editor-oboservable.ts
@@ -1,10 +1,10 @@
 import { EditPoint } from './edit-point';
 import { EditorObservable } from './editor-observable';
-import { PolygonEditUpdate } from './polygon-edit-update';
+import { HippodromeEditUpdate } from './hippodrome-edit-update';
 import { PointProps } from './point-edit-options';
 import { Cartesian3 } from '../../angular-cesium/models/cartesian3';
 
-export class HippodromeEditorObservable extends EditorObservable<PolygonEditUpdate> {
+export class HippodromeEditorObservable extends EditorObservable<HippodromeEditUpdate> {
   setManually: (firstPosition: Cartesian3,
                 secondPosition: Cartesian3,
                 widthMeters?: number,
